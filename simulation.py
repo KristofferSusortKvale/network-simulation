f57b05b5fb3fd40c5345fbea12cf21aa8c75c703
KristofferSusortKvale <kristoffersusortkvale@gmail.com>
2022-05-20 11:04:13 +0200

    Update simulation.py
    Changed simulation.py for user class

--- simulation.py
+++ simulation.py
@@ -1,4 +1,6 @@
-# from imports/classes/node.py import node
+from imports.node import node
+from imports.user import user
+from imports.my_datetime import get_now
 
 # loop variables
 end_of_simulation = False
@@ -9,9 +11,16 @@ end_time = get_now() + simulation_time
 simulation_root_directory = Path.cwd().parents[0]
 
 node = node("127.0.0.1", "unique-string", is_test)
+user = user(5, "127.0.0.2", "other-unique-string")
 
 # Actual loop
 while not end_of_simulation:
+    # users
+    if user.tick():
+        user.create_package(node)
+        user.new_task()
+
+    # nodes
     node.check_received()
     node.process_package()
     #send_package()
