b46dc94c0b1ca0ea2189d4a0d52353e35021dd9d
KristofferSusortKvale <kristoffersusortkvale@gmail.com>
2022-05-20 17:00:03 +0200

    Update simulation.py
    changes as part of testing

--- simulation.py
+++ simulation.py
@@ -1,28 +1,54 @@
 from imports.node import node
 from imports.user import user
 from imports.datetime import get_now
+from numpy.random import randint
 
 # loop variables
 end_of_simulation = False
-is_test = False
+#is_test = False
 is_one_cycle = False
 simulation_time = 10 # seconds
 end_time = get_now() + simulation_time
 
-node = node("127.0.0.1", is_test)
+node1 = node("127.0.0.1", max_send_data_packages=5, pretty_name="1")
+node2 = node("127.0.0.3", max_send_data_packages=5, pretty_name="2")
+goal = node("127.0.0.4", max_send_data_packages=5, pretty_name="goal")
 user = user("127.0.0.2", 5)
+
+# set up lookups
+node1.add_lookup_entry(node2.get_ip_address(), node2)
+node1.add_lookup_entry(goal.get_ip_address(), goal)
+node1.add_lookup_entry(user.get_ip_address(), user)
+
+node2.add_lookup_entry(node1.get_ip_address(), node1)
+node2.add_lookup_entry(goal.get_ip_address(), goal)
+node2.add_lookup_entry(user.get_ip_address(), user)
+
+goal.add_lookup_entry(user.get_ip_address(), user)
 
 # Actual loop
 while not end_of_simulation:
     # users
     if user.tick():
-        user.create_package(node)
+        random_int = randint(2)
+        if random_int == 0:
+            user.create_package(node1, goal)
+        elif random_int == 1:
+            user.create_package(node2, goal)
         user.new_task()
 
     # nodes
-    node.check_received()
-    node.process_package()
-    node.send_package()
+    node1.check_received()
+    node2.check_received()
+    goal.check_received()
+
+    node1.process_package()
+    node2.process_package()
+    goal.process_package()
+
+    node1.send_package()
+    node2.send_package()
+    goal.send_package()
 
     if is_one_cycle:
         end_of_simulation = True
@@ -30,5 +56,7 @@ while not end_of_simulation:
     if get_now() > end_time:
         end_of_simulation = True
 
-node.write_results()
+node1.write_results()
+node2.write_results()
+goal.write_results()
 user.write_results()
