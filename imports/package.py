fa48e89ad33c8f098b60f0398da2a622b0df3415
KristofferSusortKvale <kristoffersusortkvale@gmail.com>
2022-05-23 13:24:42 +0200

    Update package.py
    Added ticks counter
    added package results for number of packages in total, and average number of ticks to complete

--- imports/package.py
+++ imports/package.py
@@ -1,11 +1,13 @@
 from imports.datetime import get_now
 from os.path import join
+from numpy import sum
 
 class header:
     def __init__(self, sender, target, time):
         self._sender = sender
         self._target = target
         self._time = time
+        self._ticks_alive = 0
 
     def get_time(self):
         return self._time
@@ -15,6 +17,12 @@ class header:
 
     def get_sender(self):
         return self._sender
+
+    def get_ticks(self):
+        return self._ticks_alive
+
+    def tick(self):
+        self._ticks_alive += 1
 
 
 class payload:
@@ -41,6 +49,9 @@ class package:
     def get_payload(self):
         return self._payload
 
+    def tick(self):
+        self._header.tick()
+
 
 def ping(sender_ip_address, target_ip_address):
     t_header = header(
@@ -65,3 +76,14 @@ def data(sender_ip_address, target_ip_address, payload_data={}):
                     get_now())
     t_payload = payload(False, False, payload_data)
     return package(t_header, t_payload)
+
+def package_results(packages):
+    result_string = "### Package Results ###\n"
+    num_packages = len(packages)
+    result_string += "Number of packages = " + str(num_packages) + "\n"
+    package_ticks = [package.get_header().get_ticks()
+                        for package in packages]
+    avg_ticks = sum(package_ticks) / num_packages
+    result_string += "Average ticks alive = " + str(avg_ticks) + "\n"
+    
+    return result_string
